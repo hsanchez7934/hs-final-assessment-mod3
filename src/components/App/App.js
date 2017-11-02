21059b956e2a61420b7b9df8cdffccf837fadb67
Hector Sanchez <hsanchez7934@gmail.com>
2017-11-02 13:23:26 -0600

    Sworn member data allocation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,32 +7,26 @@ import CardContainer from '../CardContainer/CardContainer.js';
 export default class App extends Component {
 
   componentDidMount() {
-    fetch(`http://localhost:3001/api/v1/character`, {
-      method: 'POST',
-      body: JSON.stringify({ url: 'https://www.anapioficeandfire.com/api/characters/255'}),
-      mode: 'cors'
-    })
-    .then(res => console.log(res));
+    // fetch(` http://localhost:3001/api/v1/houses`, {
+    //   method: 'GET'
+    // })
+    //   .then(response => response.json())
+    //   .then(res => {
+    //     res.forEach( person => {
+    //       person.swornMembers.forEach( endpoint => {
+    //         fetch('http://www.localhost:3001/api/v1/character', {
+    //           method: 'POST',
+    //           headers: {
+    //             'Content-Type': 'application/json'
+    //           },
+    //           body: JSON.stringify({ url: endpoint })
+    //         })
+    //         .then(res => res.json())
+    //         .then(res => console.log(res));
+    //       });
+    //     });
+    //   });
   }
-
-  // fetch(` http://localhost:3001/api/v1/houses`, {
-  //   method: 'GET'
-  // })
-  //   .then(response => response.json())
-  //   .then(res => {
-  //     res.forEach( person => {
-  //       console.log(person.swornMembers);
-  //       person.swornMembers.forEach( url => {
-  //         fetch('http://localhost:3001/api/v1/character', {
-  //           method: 'POST',
-  //           body: {
-  //             url: '...url'
-  //           }
-  //         })
-  //         .then(res => console.log(res))
-  //       });
-  //     });
-  //   });
 
   render() {
     return (
@@ -46,7 +40,7 @@ export default class App extends Component {
           }}> FAKE ACTION</button>
         </div>
         <div className='Display-info'>
-          {/* <CardContainer /> */}
+          <CardContainer />
         </div>
       </div>
     );
--- src/reducers/swornmembersreducer.js
+++ src/reducers/swornmembersreducer.js
@@ -1,7 +1,7 @@
 export const swornMembers = (state = [], action) => {
   switch (action.type) {
   case 'GET_SWORN_MEMBERS':
-    return action.array;
+    return [...state, action.array];
   default:
     return state;
   }
